7dd9f94b7346b36a6673afabc7d1f26d1ad6c4db
jReig <65089964+Randymagnumm@users.noreply.github.com>
2021-02-09 17:29:32 -0300

    Agregue una pag del bootcamp
    es para ir avanzando con el otro equipo, no tiene nada magnifico

---
+++ BootCamp.php
@@ -0,0 +1,82 @@
+<?php 
+
+include('templates/header.php');
+
+?>
+  <main>
+    <section class="hero club emprendedores">
+      <div class="-blur">
+        <div class="container">
+          <div class="row valign-row-l" data-aos="fade-up">
+            <div class="col s12 m12 l5">
+              <h1>Bootcamp Experience</h1>
+              <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt minus eveniet quia dolor quasi ipsa nobis voluptatum, error architecto dolore facilis expedita id, repudiandae unde doloremque iure ut provident? Tenetur!</h5>
+            </div>
+            <div class="col s12 m12 l6 offset-l1">
+              <img src="img/bootcamp/logo.png" class="responsive-img" alt="placeholder">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section class="container section" id="section-1">
+      <h4 style="padding-bottom:20px;">Misión y Visión</h4>
+        <h5>Bootcamp good, Que queremos, acercar el mundo emprendedor a los profesionales del futuro</h5>
+
+    </section>
+
+    <section class="alt-container section" id="section-2">
+      <div class="container">
+        <h4 style="margin-bottom:50px">Trabajando con la Juventud</h4>
+      </div>
+      <div class="row valign-row-l" data-aos="fade-up">
+        <div class="col s12 m12 l6">
+          <img src="img/placeholder.jpg" class="responsive-img" alt="placeholder">
+        </div>
+        <div class="col s12 m12 l6">
+          <p>Con charlas, actividades y de manera dinamica ....</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="container section" id="section-3">
+      <h4 style="padding-bottom:20px;">Premios que cambian vidas</h4>
+        <h5>nuestros premiazos</h5>
+
+    </section>
+
+    <section class="alt-container section" id="section-4">
+      <div class="container">
+        <h4 style="margin-bottom:50px">Video del BC</h4>
+      </div>
+      <div class="row valign-row-l" data-aos="fade-up">
+        <div class="col s12 m12 l6">
+          <img src="img/placeholder.jpg" class="responsive-img" alt="placeholder">
+        </div>
+      </div>
+    </section>
+
+    <section class="container section center" id="section-5">
+      <h4>Nuestro Sponsor</h4>
+      <a class="sponsor" href="https://www.bain.com/" target="_blank" rel="noreferrer"><img src="img/sponsors/logo-bain.png" class="responsive-img"></a>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In maxime placeat assumenda eum porro sequi perferendis reprehenderit sed est, distinctio dolorum vel a sint ullam quasi minima eligendi dolorem. Ea?</p>
+    </section>
+
+    <section class="alt-container section" id="section-4">
+      <div class="container">
+        <h4 style="margin-bottom:50px">FAQ</h4>
+      </div>
+    </section>
+
+
+  </main>
+
+
+<?php 
+
+include('templates/footer.php');
+
+?>
