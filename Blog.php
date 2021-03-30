cc9270be771501c46ba2af08b7ac8b62c14f886c
jReig <65089964+Randymagnumm@users.noreply.github.com>
2021-03-30 15:37:13 -0300

    Ya la pagina del blog fachera

--- Blog.php
+++ Blog.php
@@ -29,65 +29,73 @@ include('templates/header.php');
 -->
 
 <section class="section container">
-    <div class="container">
-    <div class="container row">
+    <div class="container blog row">
 
         <div class="blog-card col s12 m12 l12">
-            <h6 class="categoria">categoria</h6>
+            <span class="categoria">Categoria</span> · <span> </span> <span class="fecha">9 de diciembre del 2018</span>
             <h4 class="titulo">Este es el título de la nota</h5>
             <h5 class="subtitulo">Un subtitulo que especifica un poco mas</h5>
             
             <div class="contenido">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
+                Feetus deleetus consectetur adipisicing elit. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
+                <img class="responsive-img" src="img/placeholder.jpg" alt="foto">
+                sit amet consectetur adipisicing elit. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
             </div>
 
-            <div class="row">
+            <div class="post-footer row">
                 <div class="left-align col s6 m6 l6">
                     <a href="">Read More</a> 
                 </div>
                 <div class="right-align col s6 m6 l6">
                     <a href="">Share <i class="fas fa-share"></i></a> 
                 </div>
-            </div>           
+            </div>     
+            <hr style="border-top: 1px solid #bbb;">      
         </div>
 
         <div class="blog-card col s12 m12 l12">
-            <h6 class="categoria">categoria</h6>
+            <span class="categoria">Categoria</span> · <span> </span> <span class="fecha">9 de diciembre del 2018</span>
             <h4 class="titulo">Este es el título de la nota</h5>
             <h5 class="subtitulo">Un subtitulo que especifica un poco mas</h5>
             
             <div class="contenido">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
+                sit amet consectetur adipisicing elit. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
+                <img class="responsive-img" src="img/placeholder.jpg" alt="foto">
+                sit amet consectetur adipisicing elit. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
             </div>
 
-            <div class="row">
+            <div class="post-footer row">
                 <div class="left-align col s6 m6 l6">
                     <a href="">Read More</a> 
                 </div>
                 <div class="right-align col s6 m6 l6">
                     <a href="">Share <i class="fas fa-share"></i></a> 
                 </div>
-            </div>           
+            </div>     
+            <hr style="border-top: 1px solid #bbb;">      
         </div>
 
         <div class="blog-card col s12 m12 l12">
-            <h6 class="categoria">categoria</h6>
+            <span class="categoria">Categoria</span> · <span> </span> <span class="fecha">9 de diciembre del 2018</span>
             <h4 class="titulo">Este es el título de la nota</h5>
             <h5 class="subtitulo">Un subtitulo que especifica un poco mas</h5>
             
             <div class="contenido">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
+                Feetus deleetus. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
+                <img class="responsive-img" src="img/placeholder.jpg" alt="foto">
+                sit amet consectetur adipisicing elit. Magnam, delectus. Sapiente exercitationem mollitia voluptatum nostrum fugit dolorum, quasi alias explicabo quos tenetur debitis possimus laudantium deserunt quo quod labore a?
             </div>
 
-            <div class="row">
+            <div class="post-footer row">
                 <div class="left-align col s6 m6 l6">
                     <a href="">Read More</a> 
                 </div>
                 <div class="right-align col s6 m6 l6">
                     <a href="">Share <i class="fas fa-share"></i></a> 
                 </div>
-            </div>           
-        </div>
+            </div>     
+            <hr style="border-top: 1px solid #bbb;">      
+        </div>        
 
     </div>
     </div>
